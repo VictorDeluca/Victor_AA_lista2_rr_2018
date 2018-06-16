9801eec462e243258d1c1b53ae29a1b66a55ff9c
VictorDeluca <ursheryl@hotmail.com>
2018-06-16 03:51:51 -0400

    Update ex4.cpp

--- ex4.cpp
+++ ex4.cpp
@@ -13,11 +13,14 @@ int main(){
 	int n,mode,i;
 	scanf("%d%d",&n,&mode);
 	if(mode==1){
+		for(i=0;i<n;i++) hashenc[i] = NULL;
+
 		for(i=0;i<n;i++){
 			int val; scanf("%d",&val);
-			node* t = hashenc[val%mod];
-			hashenc[val%mod]->val = val;
-			hashenc[val%mod]->next = t;
+			node* t = (node*)malloc(sizeof(node));
+			t->next = hashenc[val%mod];
+			t->val = val;
+			hashenc[val%mod] = t;
 		}
 	} else {
 		for(i=0;i<n;i++){
