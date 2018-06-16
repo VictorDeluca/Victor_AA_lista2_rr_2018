46dbae99944acfc8e108e2a65e9a5c2944708003
VictorDeluca <ursheryl@hotmail.com>
2018-06-16 04:17:57 -0400

    Update ex4.cpp

--- ex4.cpp
+++ ex4.cpp
@@ -13,6 +13,7 @@ int main(){
 	int n,mode,i;
 	scanf("%d%d",&n,&mode);
 	if(mode==1){
+		int start = clock();
 		for(i=0;i<n;i++) hashenc[i] = NULL;
 
 		for(i=0;i<n;i++){
@@ -22,11 +23,20 @@ int main(){
 			t->val = val;
 			hashenc[val%mod] = t;
 		}
+
+		int end = clock();
+		double dif = (end - start)*1000; dif/=CLOCKS_PER_SEC;
+		printf("%6lf\n",dif);
 	} else {
+		int start = clock();
 		for(i=0;i<n;i++){
 			int val; scanf("%d",&val);
 			hashrb[val%mod].insert(val);
 		}
+
+		int end = clock();
+		double dif = (end - start)*1000; dif/=CLOCKS_PER_SEC;
+		printf("%6lf\n",dif);
 	}
 
 	return 0;
